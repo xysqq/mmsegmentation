d06ae48eb3dd10ef1f6ae880c0df5ee58acc621b
xys <80815268+xysqq@users.noreply.github.com>
2022-11-02 23:27:29 +0800

    num_classes

--- configs2/靶区自动勾画/靶区自动勾画.py
+++ configs2/靶区自动勾画/靶区自动勾画.py
@@ -1,7 +1,7 @@
 _base_ = [
-    '../base/deeplabv3_unet_s5-d16.py', '../base/base_dicom_dataset.py',
+    '../base/fcn_unet_s5-d16.py', '../base/base_dicom_dataset.py',
     '../base/default_runtime.py', '../base/schedule_40k.py'
 ]
-model = dict(decode_head=dict(num_classes=2), auxiliary_head=dict(num_classes=2),
+model = dict(decode_head=dict(num_classes=21), auxiliary_head=dict(num_classes=21),
              test_cfg=dict(crop_size=(64, 64), stride=(42, 42)))
 evaluation = dict(metric='mDice')
