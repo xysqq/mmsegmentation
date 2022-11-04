67fba0e2a59ccf28d28ac2fa1f39b7d7040745c5
xys <80815268+xysqq@users.noreply.github.com>
2022-11-04 22:37:47 +0800

    Create 靶区自动勾画_fcn_unet_s5-d16.py

---
+++ configs2/靶区自动勾画/靶区自动勾画_fcn_unet_s5-d16.py
@@ -0,0 +1,7 @@
+_base_ = [
+    '../base/fcn_unet_s5-d16.py', '../base/base_dicom_dataset.py',
+    '../base/default_runtime.py', '../base/schedule_40k.py'
+]
+model = dict(decode_head=dict(num_classes=21), auxiliary_head=dict(num_classes=21),
+             test_cfg=dict(crop_size=(64, 64), stride=(42, 42)))
+evaluation = dict(metric='mDice')
