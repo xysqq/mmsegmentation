33252af0cf58ea9e15faabff1f527c0206d4b022
xys <80815268+xysqq@users.noreply.github.com>
2022-11-06 16:19:39 +0800

    预训练模型

--- configs2/靶区自动勾画/靶区自动勾画_fcn_unet_s5-d16.py
+++ configs2/靶区自动勾画/靶区自动勾画_fcn_unet_s5-d16.py
@@ -1,7 +1,9 @@
 _base_ = [
     '../base/fcn_unet_s5-d16.py', '../base/base_dicom_dataset.py',
-    '../base/default_runtime.py', '../base/schedule_40k.py'
+    '../base/default_runtime.py', '../base/schedule_80k.py'
 ]
 model = dict(decode_head=dict(num_classes=21), auxiliary_head=dict(num_classes=21),
              test_cfg=dict(crop_size=(64, 64), stride=(42, 42)))
 evaluation = dict(metric='mDice')
+
+load_from = "https://download.openmmlab.com/mmsegmentation/v0.5/unet/fcn_unet_s5-d16_64x64_40k_drive/fcn_unet_s5-d16_64x64_40k_drive_20201223_191051-5daf6d3b.pth"
