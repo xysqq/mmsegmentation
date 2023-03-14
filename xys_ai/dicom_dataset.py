9912b46ccf8b363be8095b882a4fc786582992c3
小徐爱吃芹菜 <80815268+xysqq@users.noreply.github.com>
2023-03-14 15:58:37 +0800

    yapf

--- xys_ai/dicom_dataset.py
+++ xys_ai/dicom_dataset.py
@@ -14,18 +14,24 @@ class DicomDataset(CustomDataset):
     """
 
     CLASSES = (
-    'background', 'BODY', 'BrainStem', 'CTV', 'CTVnd', 'Chiasm', 'Cochlea', 'Eye', 'GTV', 'GTVnd', 'Hippocampus',
-    'Lens', 'Mandible',
-    'Mastoid', 'OpticNerve', 'Parotid', 'PharynxConst', 'SpinalCord', 'Submandibular', 'TMjoint', 'TemporalLobe',
-    'Thyroid')
+        'background', 'BODY', 'BrainStem', 'CTV', 'CTVnd', 'Chiasm', 'Cochlea',
+        'Eye', 'GTV', 'GTVnd', 'Hippocampus', 'Lens', 'Mandible', 'Mastoid',
+        'OpticNerve', 'Parotid', 'PharynxConst', 'SpinalCord', 'Submandibular',
+        'TMjoint', 'TemporalLobe', 'Thyroid')
 
-    PALETTE = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0], [0, 0, 128],
-               [128, 0, 128], [0, 128, 128], [128, 128, 128], [64, 0, 0],
-               [192, 0, 0], [64, 128, 0], [192, 128, 0], [64, 0, 128],
-               [192, 0, 128], [64, 128, 128], [192, 128, 128], [0, 64, 0],
-               [128, 64, 0], [0, 192, 0], [128, 192, 0], [0, 64, 128], [64, 64, 64]]
+    PALETTE = [
+        [0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0], [0, 0, 128],
+        [128, 0, 128], [0, 128, 128], [128, 128, 128], [64, 0, 0], [192, 0, 0],
+        [64, 128, 0], [192, 128, 0], [64, 0, 128], [192, 0, 128],
+        [64, 128, 128], [192, 128, 128], [0, 64, 0], [128, 64, 0], [0, 192, 0],
+        [128, 192, 0], [0, 64, 128], [64, 64, 64]
+    ]
 
     def __init__(self, split, **kwargs):
         super(DicomDataset, self).__init__(
-            img_suffix='.jpg', seg_map_suffix='.png', split=split, reduce_zero_label=False, **kwargs)
+            img_suffix='.jpg',
+            seg_map_suffix='.png',
+            split=split,
+            reduce_zero_label=False,
+            **kwargs)
         assert osp.exists(self.img_dir) and self.split is not None
