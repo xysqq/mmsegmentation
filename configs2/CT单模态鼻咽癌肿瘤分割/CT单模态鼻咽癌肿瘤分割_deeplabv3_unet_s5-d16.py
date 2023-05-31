b14e39b37365cc286d97cb489489e11ada18e7cb
xys <80815268+xysqq@users.noreply.github.com>
2023-05-31 22:39:15 +0800

    CT单模态鼻咽癌肿瘤分割

--- configs2/CT单模态鼻咽癌肿瘤分割/CT单模态鼻咽癌肿瘤分割_deeplabv3_unet_s5-d16.py
+++ configs2/CT单模态鼻咽癌肿瘤分割/CT单模态鼻咽癌肿瘤分割_deeplabv3_unet_s5-d16.py
@@ -1,8 +1,8 @@
 _base_ = [
-    '../base/pspnet_unet_s5-d16.py', '../base/base_dicom_dataset.py',
-    '../base/default_runtime.py', '../base/schedule_40k.py'
+    '../base/deeplabv3_unet_s5-d16.py', '../base/base_dicom_dataset.py',
+    '../base/default_runtime.py', '../base/schedule_80k.py'
 ]
-model = dict(decode_head=dict(num_classes=23), auxiliary_head=dict(num_classes=23),
+model = dict(decode_head=dict(num_classes=2), auxiliary_head=dict(num_classes=2),
              test_cfg=dict(crop_size=(64, 64), stride=(42, 42)))
 evaluation = dict(metric='mDice')
 
